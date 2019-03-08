5668819f554e4f5666d014ba0f943357ddb833da
Reham Ali <47555052+rehamaali@users.noreply.github.com>
2019-03-08 23:45:10 +0200

    send message doc

--- Mini-Reddit Project/app/Http/Controllers/MessagesController.php
+++ Mini-Reddit Project/app/Http/Controllers/MessagesController.php
@@ -6,5 +6,19 @@ use Illuminate\Http\Request;
 
 class MessagesController extends Controller
 {
-    //
+    /**
+     *
+     * @group Send a Message
+     * This is used to send a message to another user.
+     * 
+     * @bodyParam userName string required The username of the sender user.
+     * @bodyParam token string required The token of the sender user.
+     * @bodyParam recUsername string required The username of the reciever user.
+     * @bodyParam msgContent string required The content of the message to be sent.
+     * 
+     */
+    public function sendMsg ($userName,$token,$recUsername,$msgContent)
+    {
+
+    }
 }
