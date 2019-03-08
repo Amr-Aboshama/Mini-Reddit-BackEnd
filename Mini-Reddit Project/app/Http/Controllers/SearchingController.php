bbae279387e8d57ec69184b6c4eb4b5286f11241
Reham Ali <47555052+rehamaali@users.noreply.github.com>
2019-03-08 23:17:37 +0200

    searchingdoc

--- Mini-Reddit Project/app/Http/Controllers/SearchingController.php
+++ Mini-Reddit Project/app/Http/Controllers/SearchingController.php
@@ -6,5 +6,18 @@ use Illuminate\Http\Request;
 
 class SearchingController extends Controller
 {
-    //
+    /**
+     *
+     * @group Search
+     * This is used to search for a community or a user.
+     * 
+     * @bodyParam userName string required The username of the current user.
+     * @bodyParam token string required The token of the current user.
+     * @bodyParam searchContent string required The string the user searching for.
+     * 
+     */
+    public function search ($userName,$token,$searchContent)
+    {
+
+    }
 }
