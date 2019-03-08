10736775e91abd6b635e1c070d4b7caef3d705d4
Reham Ali <47555052+rehamaali@users.noreply.github.com>
2019-03-09 01:40:26 +0200

    editing variables names

--- Mini-Reddit Project/app/Http/Controllers/MessagesController.php
+++ Mini-Reddit Project/app/Http/Controllers/MessagesController.php
@@ -11,13 +11,13 @@ class MessagesController extends Controller
      * @group Send a Message
      * This is used to send a message to another user.
      * 
-     * @bodyParam userName string required The username of the sender user.
+     * @bodyParam my_username string required The username of the sender user.
      * @bodyParam token string required The token of the sender user.
-     * @bodyParam recUsername string required The username of the reciever user.
-     * @bodyParam msgContent string required The content of the message to be sent.
+     * @bodyParam rec_username string required The username of the reciever user.
+     * @bodyParam msg_content string required The content of the message to be sent.
      * 
      */
-    public function sendMsg ($userName,$token,$recUsername,$msgContent)
+    public function sendMsg ($my_username,$token,$rec_username,$msg_content)
     {
 
     }
--- Mini-Reddit Project/app/Http/Controllers/SearchingController.php
+++ Mini-Reddit Project/app/Http/Controllers/SearchingController.php
@@ -11,12 +11,12 @@ class SearchingController extends Controller
      * @group Search
      * This is used to search for a community or a user.
      * 
-     * @bodyParam userName string required The username of the current user.
+     * @bodyParam my_username string required The username of the current user.
      * @bodyParam token string required The token of the current user.
-     * @bodyParam searchContent string required The string the user searching for.
+     * @bodyParam search_content string required The string the user searching for.
      * 
      */
-    public function search ($userName,$token,$searchContent)
+    public function search ($my_username,$token,$search_content)
     {
 
     }
