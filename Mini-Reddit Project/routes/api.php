9234001b94c5bf02cdcb8ae9dd1efa2467e6e92f
Reham Ali <47555052+rehamaali@users.noreply.github.com>
2019-03-08 23:45:10 +0200

    send message doc

--- Mini-Reddit Project/routes/api.php
+++ Mini-Reddit Project/routes/api.php
@@ -18,3 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('/search','SearchingController@search');
+Route::post('/sendmsg','MessagesController@sendMsg');
