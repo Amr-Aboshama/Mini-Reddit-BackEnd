f120096c691ca59f3d408d59881038f2dcd96ded
Menna_Fekry <mennafekry98@gmail.com>
2019-03-09 12:20:21 +0200

    api

---
+++ Mini-Reddit Project/app/Http/Controllers/InformationController.php
@@ -0,0 +1,39 @@
+
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class InformationController extends Controller
+{
+
+      /**
+       *
+       * APIs for managing user information 
+       * @bodyParam my_username string required the username of the current user.
+       * @bodyParam token string required the token of the user and it is required for authontication.
+       * @response 200{
+       *  "email":"john_bb@gmail"
+       * }
+       */
+      public function viewPrivateUserInfo(){
+		// ...
+	}
+
+	/**
+       *
+       * APIs for managing user information 
+       * @bodyParam my_username string required the username of the current user.
+       * @bodyParam token string required the token of the user and it is required for authontication.
+       * @response 200{
+       *  "username": "john",
+       *  "karma":500,
+       *  "cake_day":"March 8, 2019",
+       *  "about":"be or not to be"
+       * }
+       */
+	public function viewPublicUserInfo(){
+		// ...
+	}
+}
