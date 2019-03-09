a2db8fdecfb00bb6b3ab724784923e5b6a26cede
Menna_Fekry <mennafekry98@gmail.com>
2019-03-09 04:14:00 +0200

    Revert "mmmm"
    This reverts commit 7ebef3807121b0641bf9116e7dbd2a0680712f59, reversing
    changes made to b1678b5ece2bf3466d4b45bb66f4431cde42dbcd.

--- Mini-Reddit Project/routes/api.php
+++ Mini-Reddit Project/routes/api.php
@@ -18,7 +18,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 
-<<<<<<< HEAD
 Route::delete('/delete/account', 'AccountSettingsController@deleteMyAccount');
 
 Route::patch('/change/password', 'AccountSettingsController@changePassword');
@@ -39,33 +38,3 @@ Route::post('/blocking/users', 'PrivacyController@blockOrUnblockUser');
 
 
 
-=======
-Route::post('/login','AuthenticationController@login');
-Route::post('/signUp','AuthenticationController@signUp');
-Route::post('/forgetPassword','AuthenticationController@forgetPassword');
-Route::post('/resetPassword','AuthenticationController@resetPassword');
-Route::post('/viewPrivateUserInfo','InformationController@viewPrivateUserInfo');
-Route::post('/viewPublicUserInfo','InformationController@viewPublicUserInfo');
-Route::post('/viewUserCommunities','CommunitiesController@viewUserCommunities');
-Route::post('/hideOrUnhidePost','InteractingController@hideOrUnhidePost');
-Route::post('/editAPost','InteractingController@editAPost');
-Route::post('/editAComment','InteractingController@editAComment');
-Route::post('/pinOrUnpinAPost','InteractingController@pinOrUnpinAPost');
-Route::post('/addOrRemoveDownvotePost','InteractingController@addOrRemoveDownvotePost');
-Route::post('/addOrRemoveDownvoteComment','InteractingController@addOrRemoveDownvoteComment');
-
-Route::post('/viewAUserMessage','MessagesController@viewAUserMessage');
-Route::post('/viewUserSentMessages','MessagesController@viewUserSentMessages');
-Route::post('/viewUserInboxMessages','MessagesController@viewUserInboxMessages');
-
-Route::get('/search','SearchingController@search');
-Route::post('/sendMsg','MessagesController@sendMsg');
-Route::get('/commRules','CommunitiesController@viewCommRulesDesc');
-Route::patch('/editComm','CommunitiesController@editComm');
-Route::post('/createComm','CommunitiesController@createComm');
-Route::delete('/removeComm','CommunitiesController@removeComm');
-Route::patch('/addModerator','CommunitiesController@addModretorForComm');
-Route::patch('/removeModerator','CommunitiesController@removeModretorFromComm');
-Route::patch('/subscriptionComm','CommunitiesController@subscriptionComm');
-Route::patch('/savingLink','InteractingController@savingLink');
->>>>>>> bb848949e2d501e8ee169d431dccf6ce3d33dfe6
