3c7a65e177c9f6bdae418a16ca7039dee4004547
Menna_Fekry <mennafekry98@gmail.com>
2019-03-09 04:14:00 +0200

    Revert "mmmm"
    This reverts commit 7ebef3807121b0641bf9116e7dbd2a0680712f59, reversing
    changes made to b1678b5ece2bf3466d4b45bb66f4431cde42dbcd.

--- Mini-Reddit Project/app/Http/Controllers/InteractingController.php
+++ Mini-Reddit Project/app/Http/Controllers/InteractingController.php
@@ -1,4 +1,3 @@
-
 <?php
 
 namespace App\Http\Controllers;
@@ -7,203 +6,6 @@ use Illuminate\Http\Request;
 
 class InteractingController extends Controller
 {
-<<<<<<< HEAD
-=======
-	/**
-      *@group Interacting
-      * APIs for managing user Interacting
-      * @bodyParam my_username string required the username of the current user.
-      * @bodyParam token string required the token of the user and it is required for authontication.
-      * @bodyParam post_id integer required the id of the post that the user wants to hide
-      * @response 200{}
-      */
-	public function hideOrUnhidePost(){
-		// ...
-	}
-
-     /**
-      *@group Interacting
-      * APIs for managing user Interacting
-      * @bodyParam my_username string required the username of the current user.
-      * @bodyParam token string required the token of the user and it is required for authontication.
-      * @bodyParam post_id integer required the id of the post that the user wants to edit
-      * @bodyparam new_title string the new title of the post
-      * @bodyparam new_content string the new content of the post
-      * @response 200{}
-      */
-     public function editAPost(){
-          // ...
-     }
-
-     /**
-      *@group Interacting
-      * APIs for managing user Interacting
-      * @bodyParam my_username string required the username of the current user.
-      * @bodyParam token string required the token of the user and it is required for authontication.
-      * @bodyParam comment_id integer required the id of the comment that the user wants to edit
-      * @bodyparam new_content string the new content of the comment
-      * @response 200{}
-      */
-     public function editAComment(){
-          // ...
-     }
-
-     /**
-      *@group Interacting
-      * APIs for managing user Interacting
-      * @bodyParam my_username string required the username of the current user.
-      * @bodyParam token string required the token of the user and it is required for authontication.
-      * @bodyParam post_id integer required the id of the post that the user wants to pin
-      * @response 200{}
-      */
-     public function pinOrUnpinAPost(){
-          // ...
-     }
-
-     /**
-      *@group Interacting
-      * APIs for managing user Interacting
-      * @bodyParam my_username string required the username of the current user.
-      * @bodyParam token string required the token of the user and it is required for authontication.
-      * @bodyParam post_id integer required the id of the post that the user wants to downvote
-      * @response 200{}
-      */
-     public function addOrRemoveDownvotePost(){
-          // ...
-     }
-
-     /**
-      *@group Interacting
-      * APIs for managing user Interacting
-      * @bodyParam my_username string required the username of the current user.
-      * @bodyParam token string required the token of the user and it is required for authontication.
-      * @bodyParam comment_id integer required the id of the comment that the user wants to downvote
-      * @response 200{}
-      */
-     public function addOrRemoveDownvoteComment(){
-          // ...
-     }
-
-	/**
-		* viewing the posts of a specific user or a community or both when you are on the home page.
-
-	 	* @bodyParam my_username string required The username of the account owner. 
- 		* @bodyParam username string if you visited another user profile this is his username.
- 		* @bodyParam token string required.
- 		* @bodyParam community_id int if you want to show the posts of a specific community this is its id. 
-
- 		*@response {
-
- 			"posts" :[ { "post_id": 1 , "body" : "post1" ,"username": "ahmed" , "votes" : 15 , "date":" 2 days ago " , "comments_num" : 0 } , { "post_id": 2 , "body" : "post2" ,"username": "ahmed" , "votes" : 15 , "date":" 2 days ago " , "comments_num" : 0 } , { "post_id": 3 , "body" : "post3" ,"username": "ahmed" , "votes" : 15 , "date":" 2 days ago " , "comments_num" : 0 }]
- 		}
-
- 		*@response 404 {
-	
-			"message" :"somethimg wrong!!!!"
- 		}
- 		
- 	*/
-
-    public function ViewPosts()
-    {
-
-    } 
-
-
-    /**
-    	* Viewing comments of a user on posts he/she has interacted with.
-
-    	*@bodyParam my_username string required The username of the account owner. 
-    	*@bodyParam username string required if you visited another user profile this is his username.
-    	*@bodyParam token string 
-
-    	*@response {
-
- 			"comments" :[ { "comment_id": 1 , "body" : "comment1" ,"username": "ahmed" , "votes" : 15 , "date":" 2 days ago " , "comments_num" : 0  } , { "comment_id": 2 , "body" : "comment2" ,"username": "ahmed" , "votes" : 15 , "date":" 2 days ago " , "comments_num" : 0  } , { "comment_id": 3 , "body" : "comment3" ,"username": "ahmed" , "votes" : 15 , "date":" 2 days ago " , "comments_num" : 0 }]
- 		}
-
- 		*@response 404 {
-	
-			"message" :"somethimg wrong!!!!"
- 		}
-    */
-
-    public function ViewComments( )
-    {
-
-    }
-
-
-
-    /**
-		*viewing comments of a specific post or replies of a specific comment
-		
-		*bodyParam my_username string required The username of the account owner.
-		*bodyParam id int required the id of the post or the id of the comment.
-		*bodyParam token string 	
-
-		*@response {
-
- 			"comments" :[ { "comment_id": 1 , "body" : "comment1" ,"username": "ahmed" , "votes" : 15 , "date":" 2 days ago " , "comments_num" : 0  } , { "comment_id": 2 , "body" : "comment2" ,"username": "ahmed" , "votes" : 15 , "date":" 2 days ago " , "comments_num" : 0  } , { "comment_id": 3 , "body" : "comment3" ,"username": "ahmed" , "votes" : 15 , "date":" 2 days ago " , "comments_num" : 0 }]
- 		}
-
- 		*@response 404 {
-	
-			"message" :"somethimg wrong!!!!"
- 		}
-
-
-    */
-
-
-    public function ViewComments_RepliesOfPosts_Comments( )
-    {
-
-    }
-
-    /**
-		*view the upvoted posts of the user or the downvoted ones
-
-		*bodyParam my_username string required The username of the account owner.
-		*bodyParam type int required it is one for the upvoted posts and zero for the downvoted ones.
-		*bodyParam token 
-
-		*@response {
-
- 			"posts" :[ { "post_id": 1 , "body" : "post1" ,"username": "ahmed" , "votes" : 15 , "date":" 2 days ago " , "comments_num" : 0  } , { "post_id": 2 , "body" : "post2" ,"username": "ahmed" , "votes" : 15 , "date":" 2 days ago " , "comments_num" : 0 } , { "post_id": 3 , "body" : "post3" ,"username": "ahmed" , "votes" : 15 , "date":" 2 days ago " , "comments_num" : 0 }]
- 		}
-
- 		*@response 404 {
-	
-			"message" :"somethimg wrong!!!!"
- 		}
-
-
-
-
-    */
-
-    public function ViewUpVotedOrDownVotedPosts( )
-    {
-
-    }
-
-
-	/**
-		*view the overview of the user.
-
-		*bodyParam my_username string required The username of the account owner.
-		*bodyParam token 
-
-    */
-
-
-    public function ViewOverview( )
-    {
-
-    }
-
->>>>>>> bb848949e2d501e8ee169d431dccf6ce3d33dfe6
     /**
      * @bodyParam my_username string required the username of the current user
      * @bodyParam token string required the token of the current user
@@ -220,7 +22,6 @@ class InteractingController extends Controller
 	}
 
     /**
-<<<<<<< HEAD
      * @bodyParam my_username string required the username of the current user
      * @bodyParam token string required the token of the current user
      * @bodyParam post_ID int required the ID of the post
@@ -229,45 +30,16 @@ class InteractingController extends Controller
      */
     
     public function pinAndUnPinPost()
-=======
-		*view the saved links by the user.
-		*bodyParam MyUserName string required The username of the account owner.
-		*bodyParam Token
-		*bodyParam my_username string required The username of the account owner.
-		*bodyParam token
-		*@response {
-
- 			"links" :[ { "link_id": 1 , "body" : "post1" ,"username": "ahmed" , "votes" : 15 , "date":" 2 days ago " , "comments_num" : 0  } , { "link_id": 2 , "body" : "post2" ,"username": "ahmed" , "votes" : 15 , "date":" 2 days ago " , "comments_num" : 0  } , { "link_id": 3 , "body" : "post3" ,"username": "ahmed" , "votes" : 15 , "date":" 2 days ago " , "comments_num" : 0 }]
- 		}
- 		*@response 404 {
-	
-			"message" :"somethimg wrong!!!!"
- 		} 
-    */
-
-    public function ViewSavedLinks( )
->>>>>>> bb848949e2d501e8ee169d431dccf6ce3d33dfe6
     {
         // ...
     }
 
-<<<<<<< HEAD
      /**
      * @bodyParam my_username string required the username of the current user
      * @bodyParam token string required the token of the current user
      * @bodyParam link_ID int required the ID of the link
      * @response 200
      * @response 400 {"message": "you are not authorized to remove the link"}
-=======
-    /**
-     * 
-     * This is used to save or unsave a post or a comment.
-     * 
-     * @bodyParam my_username string required The username of the current user.
-     * @bodyParam token string required The token of the current user.
-     * @bodyParam link_id int required The ID of the post/comment to be saved or unsaved.
-     * 
->>>>>>> bb848949e2d501e8ee169d431dccf6ce3d33dfe6
      */
     
     public function removeLink()
@@ -277,13 +49,3 @@ class InteractingController extends Controller
 
 
 }
-<<<<<<< HEAD
-=======
-
-
-
-
-
-
-
->>>>>>> bb848949e2d501e8ee169d431dccf6ce3d33dfe6
