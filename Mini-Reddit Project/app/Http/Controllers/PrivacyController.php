bf5f4d350d77920552d2ef5478e6f631ec0bc95f
Amr-Aboshama <31421825+Amr-Aboshama@users.noreply.github.com>
2019-03-09 19:40:40 +0200

    Finishing Controllers [Amr's Part]
    - Authentication Controller
    - Following Controller
    - Privacy Controller

--- Mini-Reddit Project/app/Http/Controllers/PrivacyController.php
+++ Mini-Reddit Project/app/Http/Controllers/PrivacyController.php
@@ -7,10 +7,10 @@ use Illuminate\Http\Request;
 class PrivacyController extends Controller
 {
 		/**
-		 * Show current user blocklisted
+		 * Show current user blocklist
 		 * @authenticated
 	   * @response 200 {
-		 * 	"success": "true"
+		 * 	"success": "true",
 	   * 	"blockedList": ["John Smith"]
 	   * }
 	   *
@@ -28,14 +28,20 @@ class PrivacyController extends Controller
 		/**
 		 *
 		 * Block a user
+		 * @authenticated
 	   * @bodyParam username string required the username of the user being blocked
 	   * @response 200 {
 	   * 	"sucess": "true"
 	   * }
 	   *
-	   *
      * @response 401 {
+	   * 	"sucess": "true",
      * 	"error": "UnAuthorized"
+     * }
+     *
+     * @response 403 {
+	   * 	"sucess": "true",
+     * 	"error": "Already blocked"
      * }
 	   */
 		public function blockUser()
@@ -46,14 +52,20 @@ class PrivacyController extends Controller
 		/**
 		 *
 		 * Unblock a user
+		 * @authenticated
 	   * @bodyParam username string required the username of the user being unblocked
 	   * @response 200 {
-	   * 	"succes": "true"
+	   * 	"sucess": "true"
 	   * }
 	   *
-	   *
      * @response 401 {
+	   * 	"sucess": "true",
      * 	"error": "UnAuthorized"
+     * }
+     *
+     * @response 403 {
+	   * 	"sucess": "true",
+     * 	"error": "Already unblocked"
      * }
 	   */
 		public function unblockUser()
@@ -63,10 +75,17 @@ class PrivacyController extends Controller
 
 
 		/**
-	   * @bodyParam my_username string required the username of the current user
-	   * @bodyParam token string required the token of the current user
-	   * @bodyParam display_name string required the username of the user being un/blocked
-	   * @response 200
+		 * Update current user Displayed Name
+		 * @authenticated
+	   * @bodyParam name string required The new name of user to update.
+	   * @response 200 {
+	   * 	"success": "true"
+	   * }
+	   *
+     * @response 401 {
+	   * 	"sucess": "true",
+     * 	"error": "UnAuthorized"
+     * }
 	   */
 		public function updateDisplayName()
 		{
@@ -75,10 +94,17 @@ class PrivacyController extends Controller
 
 
 		/**
-	   * @bodyParam my_username string required the username of the current user
-	   * @bodyParam token string required the token of the current user
-	   * @bodyParam about string required the username of the user being un/blocked
-	   * @response 200
+		 * Update current user About
+		 * @authenticated
+	   * @bodyParam about string required the content of about to be updated to
+	   * @response 200 {
+	   * 	"success": "true"
+	   * }
+	   *
+     * @response 401 {
+	   * 	"sucess": "true",
+     * 	"error": "UnAuthorized"
+     * }
 	   */
 		public function updateAbout()
 		{
