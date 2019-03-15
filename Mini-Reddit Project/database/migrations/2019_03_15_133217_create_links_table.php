028a6ba742983fd1a11b4aef827e35a92daa2ead
ahmed <ahmed essam@DESKTOP-R7HC7K6.(none)>
2019-03-15 20:46:23 +0200

    migrations except notifications

---
+++ Mini-Reddit Project/database/migrations/2019_03_15_133217_create_links_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLinksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('links', function (Blueprint $table) {
+
+
+            $table->bigIncrements('link_id');
+            $table->string('content')->nullable();
+            $table->string('content_image')->nullable(); //url of the image
+            $table->string('title')->nullable();
+            $table->dateTime('link_date');
+            $table->unsignedTinyInteger('pinned');
+            $table->string('author_user_name');     //owner of the post....
+            $table->unsignedBigInteger('community_id')->nullable();     //if the post in community...
+            $table->unsignedBigInteger('parent_id')->nullable();
+
+            //restrictions
+
+            $table->foreign('author_user_name')->references('user_name')->on('users')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('parent_id')->references('link_id')->on('links')->onUpdate('cascade')->onDelete('cascade');
+
+
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('links');
+    }
+}
---
+++ Mini-Reddit Project/database/migrations/2019_03_15_175111_create_upvoted_posts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUpvotedPostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('upvoted_posts', function (Blueprint $table) {
+
+          $table->string( 'user_name' );
+          $table->unsignedBigInteger('link_id');
+
+          //restrictions
+
+          $table->foreign('user_name')->references('user_name')->on('users')->onUpdate('cascade')->onDelete('cascade');
+          $table->foreign('link_id')->references('link_id')->on('links')->onUpdate('cascade')->onDelete('cascade');
+          $table->primary(['link_id','user_name']);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('upvoted_posts');
+    }
+}
