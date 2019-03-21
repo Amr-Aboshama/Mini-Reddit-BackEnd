2760a8aed5ad14925bc4d1059f4944d8ff6cb25a
Amr-Aboshama <31421825+Amr-Aboshama@users.noreply.github.com>
2019-03-21 22:44:35 +0200

    Adding Tests
    - Sign in test
    - Sign up test
    - Sign out test

---
+++ Mini-Reddit Project/tests/Unit/SignInTest.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
+
+class SignInTest extends TestCase
+{
+    /**
+     * Test invalid signin
+     * -> Required data
+     */
+    public function testInvalidSignIn()
+    {
+        $this->MissedResponse(['username' => null, 'password' => null]);
+        $this->MissedResponse(['username' => 'amroo', 'password' => null]);
+        $this->MissedResponse(['username' => null, 'password' => '123456789']);
+        $this->MissedResponse(['username' => 'amroo', 'password' => '1234567']);
+        $this->MissedResponse(['username' => 'amr', 'password' => '12345678']);
+    }
+
+    /**
+     * Test invalid signin
+     * -> Mismatch of username and password
+     */
+    public function testUsernameAndPasswordMismatch()
+    {
+        $user = User::storeUser(['username' => 'testo','password' => 'armne123456','email' => 'testo@test.com']);
+
+        $payload = ['username' => 'testo' . 'x', 'password' => 'armne123456'];
+
+        $this->json('POST','api/unauth/signIn',$payload)
+            ->assertStatus(404)
+            ->assertJson([
+              'success' => 'false',
+              'error' => 'username and password don\'t matched'
+            ]);
+
+        $user->delete();
+    }
+
+    /**
+     * Test for a successful signin
+     */
+    public function testValidSignIn()
+    {
+
+        $user = User::storeUser(['username' => 'testo','password' => 'armne123456','email' => 'testo@test.com']);
+
+        $payload = ['username' => 'testo', 'password' => 'armne123456'];
+
+        $this->json('POST','api/unauth/signIn',$payload)
+            ->assertStatus(200)
+            ->assertJson([
+              'success' => 'true'
+            ])
+            ->assertJsonStructure([
+              'token'
+            ]);
+      $user->delete();
+    }
+
+    /**
+     * Utilitiy function to test invalid signin
+     * $payload => Parameters of the request
+     */
+    public function MissedResponse($payload)
+    {
+        $this->json('POST','api/unauth/signIn',$payload)
+            ->assertStatus(422)
+            ->assertJson([
+              'success' => 'false',
+              'error' => 'Invalid or some data missed'
+            ]);
+    }
+}
---
+++ Mini-Reddit Project/tests/Unit/SignUpTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
+
+class SignUpTest extends TestCase
+{
+    /**
+     * Test for invalid data for signup
+     * -> Required data
+     * -> Minimum Number of characters
+     * -> Unique username and email
+     * -> Password confirmation
+     */
+    public function testInvalidSignUp()
+    {
+       $this->MissedResponse(['username' => null, 'password' => null, 'email' => null, 'password_confirmation' => null]);
+       $this->MissedResponse(['username' => null, 'password' => '123456789', 'email' => 'a@a.com', 'password_confirmation' => '123456789']);
+       $this->MissedResponse(['username' => 'testo', 'password' => null, 'email' => 'a@a.com', 'password_confirmation' => '123456789']);
+       $this->MissedResponse(['username' => 'testo', 'password' => '123456789', 'email' => null, 'password_confirmation' => '123456789']);
+       $this->MissedResponse(['username' => 'tes', 'password' => '123456789', 'email' => 'a@a.com', 'password_confirmation' => '123456789']);
+       $this->MissedResponse(['username' => 'testo', 'password' => '123456789', 'email' => 'a@a.com', 'password_confirmation' => '23456789']);
+    }
+
+    /**
+     * Test for a successful signup
+     */
+    public function testValidSignUp()
+    {
+        $payload = ['username' => 'testo', 'password' => '123456789', 'email' => 'testo@test.com', 'password_confirmation' => '123456789'];
+        $this->json('POST','api/unauth/signUp',$payload)
+            ->assertStatus(200)
+            ->assertJson([
+              'success' => 'true'
+            ])
+            ->assertJsonStructure([
+              'token'
+            ]);
+       User::deleteUserByUsername('testo');
+    }
+
+    /**
+     * Utilitiy function to test invalid signup
+     * $payload => Parameters of the request
+     */
+    public function MissedResponse($payload)
+    {
+        $this->json('POST','api/unauth/signUp',$payload)
+            ->assertStatus(422)
+            ->assertJson([
+              'success' => 'false',
+              'error' => 'Invalid or some data missed'
+              ]);
+    }
+}
