65a214e2af0e2696a2cf164dad1aaaf7ec005f0b
Reham Ali <47555052+rehamaali@users.noreply.github.com>
2019-03-22 23:34:28 +0200

    pinorunpin api &moderate community

--- Mini-Reddit Project/app/ModerateCommunity.php
+++ Mini-Reddit Project/app/ModerateCommunity.php
@@ -3,8 +3,27 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class ModerateCommunity extends Model
 {
-    //
+    protected $fillable = ['username','community_id'];
+    public $timestamps = false; //so that doesn't expext time columns
+
+
+    public static function checkExisting($community_id,$username)
+    {
+        $result = ModerateCommunity::where('community_id', $community_id)->where('username',$username)->exists();
+        return $result;
+    }
+
+    public static function store($community_id,$username)
+    {
+        try {
+            ModerateCommunity::create(['username' => $username , 'community_id' => $community_id ]);
+            return true;
+        } catch (\Exception $e) {
+            return false;
+        }
+    }
 }
