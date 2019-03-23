92dadb84686366b05c2703149031015dda8b2f7d
ahmed <ahmed essam@DESKTOP-R7HC7K6.(none)>
2019-03-24 01:30:20 +0200

    errors fixed

--- Mini-Reddit Project/app/Blocking.php
+++ Mini-Reddit Project/app/Blocking.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Blocking extends Model
 {
-     public static function blokedOrBlocker($username1 , $username2)
+     public static function blockedOrBlocker($username1 , $username2)
      {
           $result1 = Blocking::where('blocker_username', $username1)->where('blocked_username', $username2)->exists();
           $result2 = Blocking::where('blocker_username', $username2)->where('blocked_username', $username1)->exists();
