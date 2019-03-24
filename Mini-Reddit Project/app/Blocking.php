7b527abf578b94f986644c1913ab8c37019a5974
Menna_Fekry <mennafekry98@gmail.com>
2019-03-24 03:43:24 +0200

    function documentation

--- Mini-Reddit Project/app/Blocking.php
+++ Mini-Reddit Project/app/Blocking.php
@@ -6,6 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Blocking extends Model
 {
+  /**
+     * function to get a list of the blocked users by a specific user 
+     *
+     * @param   string  $username  
+     *
+     * @return  Array    array containing blocked usernames
+     */	
   public static function getUsersBlockedByUsername ($username)
   {
       return Blocking::where('blocker_username', '=',  $username )
@@ -13,6 +20,13 @@ class Blocking extends Model
           -> pluck('blocked_username');
   }
 
+  /**
+     * function to get a list of the users uwho blocked a specific user   
+     *
+     * @param   string  $username the user who is blocked  
+     *
+     * @return  Array    array containing usernames who blocked $username
+     */
   public static function getUsersWhoBlockedUsername ($username)
   {
      return Blocking::where('blocked_username', '=',  $username )
@@ -20,6 +34,15 @@ class Blocking extends Model
           -> pluck('blocker_username');
   }
 
+  /**
+     * function to make a user block another user   
+     *
+     * @param   string  $blocker_username
+     *
+     * @param   string   $blocked_username
+     *
+     * @return  object   record of the blocking table 
+     */
   public static function BlockUser($blocker_username, $blocked_username)
   {
       return Blocking::create([
@@ -27,6 +50,16 @@ class Blocking extends Model
         'blocked_username' => $blocked_username
       ]);
   }
+
+     /**
+     * function to check if username1 is blocking another username2 or vice versa   
+     *
+     * @param   string  $username1
+     *
+     * @param   string  $username1
+     *
+     * @return  bool true if anyone is blocking the other, false if not    
+     */
      public static function blockedOrBlocker($username1 , $username2)
      {
           $result1 = Blocking::where('blocker_username', $username1)->where('blocked_username', $username2)->exists();
