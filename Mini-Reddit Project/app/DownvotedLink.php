1bf6260a02f3a249003aa28e22f69a4a6d02a2e3
ahmed <ahmed essam@DESKTOP-R7HC7K6.(none)>
2019-05-04 04:24:09 +0200

    Revert "Revert "Revert "Revert "Merge branch 'Development' of https://github.com/Amr-Aboshama/Mini-Reddit-BackEnd into Development""""
    This reverts commit 9faf763f7b5d4b26dfe23210fa43e2009c137029.

--- Mini-Reddit Project/app/DownvotedLink.php
+++ Mini-Reddit Project/app/DownvotedLink.php
@@ -10,17 +10,17 @@ class DownvotedLink extends Model
     public $timestamps = false; //so that doesn't expext time columns
 
     /**
-     * this function creats a new record into the database relation called 'downvoted_links' given the username of the user and the id of the link which he/she upvoted
+     * this function creats a new record into the database relation called 'downvoted_links' given the username of the user and the id of the link which he/she upvoted.
      *
-     * @param   string  $username  the username of the user who presses downvote link
-     * @param   int  $post_id   the id of the link to be downvoted by the user
+     * @param string $username the username of the user who presses downvote link
+     * @param int    $post_id  the id of the link to be downvoted by the user
      *
-     * @return  boolean             [ true if the creation succeeded and false if it failed ]
+     * @return bool [ true if the creation succeeded and false if it failed ]
      */
     public static function store($username, $post_id)
     {
         try {
-            DownvotedLink::create(['username' => $username , 'link_id' => $post_id ]);
+            self::create(['username' => $username, 'link_id' => $post_id]);
 
             return true;
         } catch (\Exception $e) {
@@ -28,35 +28,33 @@ class DownvotedLink extends Model
         }
     }
 
-
     /**
      * this function is to remove a recored from  the database relation called 'downvoted_links' given the username of the user and the id of the link
-     * given the username and post_id
+     * given the username and post_id.
      *
-     * @param   string  $username  the username of the user who downvoted the link
-     * @param   int  $post_id   the id of the link to be undownvoted by the user
+     * @param string $username the username of the user who downvoted the link
+     * @param int    $post_id  the id of the link to be undownvoted by the user
      *
-     * @return  boolean             [ true if the record deleted successfully and false if failed ]
+     * @return bool [ true if the record deleted successfully and false if failed ]
      */
     public static function remove($username, $post_id)
     {
-        $result = DownvotedLink::where('username', $username)->where('link_id', $post_id)->delete();
+        $result = self::where('username', $username)->where('link_id', $post_id)->delete();
 
         return $result;
     }
 
-
     /**
      * this function checks if a specific user downvoted a specific link given its id and the username of the user.
      *
-     * @param   int  $link_id   the id of the link
-     * @param   string  $username  the username of the user
+     * @param int    $link_id  the id of the link
+     * @param string $username the username of the user
      *
-     * @return  boolean             [ true if the user downvotes the link and false if not ]
+     * @return bool [ true if the user downvotes the link and false if not ]
      */
     public static function downvoted($link_id, $username)
     {
-        $result = DownvotedLink::where('link_id', $link_id)->where('username', $username)->exists();
+        $result = self::where('link_id', $link_id)->where('username', $username)->exists();
 
         return $result;
     }
--- Mini-Reddit Project/app/UpvotedLink.php
+++ Mini-Reddit Project/app/UpvotedLink.php
@@ -12,15 +12,15 @@ class UpvotedLink extends Model
     /**
      * this function creats a new record into the database relation called 'upvoted_links'.
      *
-     * @param   string  $username  the username of the user who presses upvote link
-     * @param   int  $post_id   the id of the link to be upvoted by the user
+     * @param string $username the username of the user who presses upvote link
+     * @param int    $post_id  the id of the link to be upvoted by the user
      *
-     * @return  boolean             [ true if the creation succeeded and false if it failed ].
+     * @return bool [ true if the creation succeeded and false if it failed ].
      */
     public static function store($username, $link_id)
     {
         try {
-            UpvotedLink::create(['username' => $username , 'link_id' => $link_id ]);
+            self::create(['username' => $username, 'link_id' => $link_id]);
 
             return true;
         } catch (\Exception $e) {
@@ -28,35 +28,33 @@ class UpvotedLink extends Model
         }
     }
 
-
     /**
      * this function is to remove a recored from  the database relation called 'upvoted_links'
      * given the username and post_id.
      *
-     * @param   string  $username  the username of the user who upvoted the link
-     * @param   int  $post_id   the id of the link to be unupvoted by the user
+     * @param string $username the username of the user who upvoted the link
+     * @param int    $post_id  the id of the link to be unupvoted by the user
      *
-     * @return  boolean            [  true if the record deleted successfully and false if failed ].
+     * @return bool [  true if the record deleted successfully and false if failed ].
      */
     public static function remove($username, $link_id)
     {
-        $result = UpvotedLink::where('username', $username)->where('link_id', $link_id)->delete();
+        $result = self::where('username', $username)->where('link_id', $link_id)->delete();
 
         return $result;
     }
 
-
     /**
      * this function checks if a specific user upvoted a specific link.
      *
-     * @param   int  $link_id   the id of the link
-     * @param   string  $username  the username of the user
+     * @param int    $link_id  the id of the link
+     * @param string $username the username of the user
      *
-     * @return  boolean             [ true if the user upvotes the link and false if not ].
+     * @return bool [ true if the user upvotes the link and false if not ].
      */
     public static function upvoted($link_id, $username)
     {
-        $result = UpvotedLink::where('link_id', $link_id)->where('username', $username)->exists();
+        $result = self::where('link_id', $link_id)->where('username', $username)->exists();
 
         return $result;
     }
